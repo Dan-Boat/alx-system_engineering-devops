6de69c336733913164d959bd384b2ad414755795
Daniel Boateng <dannboateng@gmail.com>
2024-01-16 22:57:19 +0100

    fixing cases

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,15 @@
+#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
+
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    if not isinstance(subreddit, str):
-        return 0
-    
-    url_api = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'safari:holberton/0.1.0'}
-    
+    if (type(subreddit) != str):
+        return(0)
+    url_api = ("https://www.reddit.com/r/{}/about.json".format(subreddit))
+    headers = {'user-agent': 'safari:holberton/0.1.0'}
     response = requests.get(url_api, headers=headers)
-    
     if response.status_code != 200:
-        return 0
-    
-    try:
-        data = response.json()["data"]
-        subscribers = data["subscribers"]
-        return subscribers
-    except (KeyError, ValueError):
-        return 0
-
-
+        return(0)
+    return(response.json().get("data").get("subscribers"))
